7aa8c5d98e26880cb8b6f14495f2277b69f190c9
yanagisawa-253 <anaguma253@gamil.com>
2021-10-22 11:13:54 +0000

    [Add]admin/postコントローラー定義

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,41 @@
+class Admin::PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post)..permit(:post_name, :post_introdction, :max_price, :min_price, :price)
+  end
+end
