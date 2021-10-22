198bf3db9de8613bff5bd7c2126c19417e8e7a73
yanagisawa-253 <anaguma253@gamil.com>
2021-10-22 11:49:34 +0000

    [update]navbar編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   root to: 'homes#top'
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/homes/about' => 'homes#about'
+
+  resources :posts
 end
