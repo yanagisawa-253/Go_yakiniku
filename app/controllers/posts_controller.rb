7b095920069458e940d6cdfbf3da5a2206ceedc7
yanagisawa-253 <anaguma253@gamil.com>
2021-11-08 11:30:33 +0000

    [update]検索機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
-  
+
   def index
     @posts = Post.all
+    @q = Post.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
